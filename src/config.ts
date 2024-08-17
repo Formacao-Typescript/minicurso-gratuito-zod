f77d931f010b0645513a23c53a76ee3ccd81b110
Lucas Santos <hello@lsantos.dev>
2024-08-18 01:50:56 +0200

    fix: remove db location

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,6 @@ import { z } from 'zod';
 const configSchema = z.object({
     PORT: z.coerce.number().default(3000),
     NODE_ENV: z.string().default('development'),
-    DB_LOCATION_PATH: z.string().default('db.json'),
 });
 
 export type AppConfig = z.infer<typeof configSchema>;
